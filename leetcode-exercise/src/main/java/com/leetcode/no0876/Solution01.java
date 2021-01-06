2d87083369f60ebc3a6a322ffe318f6c4239f985
SoSo <LuckilyChensy@163.com>
2021-01-06 23:02:29 +0800

    leetcode - no 876

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0876/Solution01.java
@@ -0,0 +1,25 @@
+package com.leetcode.no0876;
+
+import com.leetcode.bean.ListNode;
+
+public class Solution01 {
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode p = head;
+
+        int len = 0;
+
+        for(;p!=null;p = p.next){
+            len++;
+        }
+
+        p = head;
+
+        for(int i = 0; i <len/2; i++){
+            p = p.next;
+        }
+
+        return p;
+    }
+}
