9500697cfa8697b8eb3dadc04769431edf5c639b
SoSo <LuckilyChensy@163.com>
2021-01-07 21:50:48 +0800

    leetcode

---
+++ leetcode-exercise/src/main/java/com/leetcode/bean/Node.java
@@ -0,0 +1,17 @@
+package com.leetcode.bean;
+
+public class Node {
+
+    public String val;
+    public Node next;
+    public Node random;
+
+    public Node(String val){}
+
+    public Node(String val,Node next,Node random) {
+        this.val = val;
+        this.next = next;
+        this.random = random;
+    }
+
+}
