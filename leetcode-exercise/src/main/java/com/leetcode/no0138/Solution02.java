7ddee2092bb0e1891d61b698de9d835fb242cdda
SoSo <LuckilyChensy@163.com>
2021-01-07 21:21:27 +0800

    leetcode - no 138

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0138/Solution02.java
@@ -0,0 +1,50 @@
+package com.leetcode.no0138;
+
+import com.leetcode.bean.Node;
+
+import java.util.HashMap;
+
+public class Solution02 {
+
+    HashMap<Node, Node> visited = new HashMap<Node,Node>();
+
+    public Node getCloneNode(Node node){
+
+        if(node != null){
+
+            if(this.visited.containsKey(node)){
+                return this.visited.get(node);
+            }else{
+                this.visited.put(node,new Node(node.val,null,null));
+                return this.visited.get(node);
+            }
+
+        }
+
+        return null;
+    }
+
+    public Node copyRandomList(Node head){
+
+        if(head == null){
+            return null;
+        }
+
+        Node oldNode = head;
+
+        Node newNode = new Node(oldNode.val);
+
+        this.visited.put(oldNode,newNode);
+
+        while(oldNode != null){
+            newNode.random =  this.getCloneNode(oldNode.random);
+            newNode.random = this.getCloneNode(oldNode.next);
+
+            oldNode = oldNode.next;
+            newNode = newNode.next;
+        }
+
+        return this.visited.get(head);
+
+    }
+}
