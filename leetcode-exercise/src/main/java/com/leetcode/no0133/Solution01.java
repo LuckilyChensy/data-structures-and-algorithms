a4b2f63fc550733d87ce00d84022e7122ec20519
SoSo <LuckilyChensy@163.com>
2021-01-07 22:04:57 +0800

    leetcode - no133

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0133/Solution01.java
@@ -0,0 +1,29 @@
+package com.leetcode.no0133;
+
+import com.leetcode.bean.GraphNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution01 {
+
+    private Map<GraphNode,GraphNode> map = new HashMap<>();
+
+    public GraphNode cloneGraph(GraphNode node) {
+        if(node == null){
+            return null;
+        }
+
+        GraphNode copy = new GraphNode(node.val);
+        map.put(node,copy);
+        for(GraphNode n:copy.neighbors){
+            if(map.containsKey(n)) {
+                copy.neighbors.add(map.get(n));
+            }else{
+                copy.neighbors.add(cloneGraph(n));
+            }
+        }
+        return copy;
+    }
+
+}
