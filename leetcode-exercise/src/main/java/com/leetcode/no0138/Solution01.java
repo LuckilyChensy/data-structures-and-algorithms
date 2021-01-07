27c3b73582e6efe4028ebe47fd500704276f4cbf
SoSo <LuckilyChensy@163.com>
2021-01-07 21:21:19 +0800

    leetcode - no 138

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0138/Solution01.java
@@ -0,0 +1,30 @@
+package com.leetcode.no0138;
+
+import com.leetcode.bean.Node;
+
+import java.util.HashMap;
+
+public class Solution01 {
+
+    HashMap<Node,Node> visitedHash = new HashMap<Node,Node>();
+
+    public Node copyRandomList(Node head){
+
+        if(head == null){
+            return null;
+        }
+
+        if(this.visitedHash.containsKey(head)){
+            return this.visitedHash.get(head);
+        }
+
+        Node node = new Node(head.val,null,null);
+
+        this.visitedHash.put(head,node);
+
+        node.next = this.copyRandomList(head.next);
+        node.random = this.copyRandomList(head.random);
+
+        return node;
+    }
+}
