95d733251c293f5cb99f88ccde2206f48487a636
SoSo <LuckilyChensy@163.com>
2021-01-18 23:18:44 +0800

    leetcode - no0572 另一个树的子树

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0572/Solution02.java
@@ -0,0 +1,34 @@
+package com.leetcode.no0572;
+
+import com.leetcode.bean.TreeNode;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution02 {
+
+    private Map<TreeNode,Integer> map = new HashMap<>();
+
+    private String computeHash(TreeNode t){
+
+        if(t == null){  return "#"; }
+
+        String left = computeHash(t.left);
+        String right = computeHash(t.right);
+        String hash = left + t.val + right;
+        map.put(t,hash.hashCode());
+        return hash;
+    }
+
+    public boolean isSub(TreeNode s,TreeNode t){
+
+        if(t == null){return true;}
+        if(s == null){return false;}
+        return map.get(s).equals(map.get(t))||isSub(s.left,t)||isSub(s.right,t);
+
+    }
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        computeHash(s);
+        computeHash(t);
+        return isSub(s,t);
+    }
+}
