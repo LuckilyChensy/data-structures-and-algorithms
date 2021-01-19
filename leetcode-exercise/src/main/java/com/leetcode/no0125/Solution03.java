d6dd61564f4cb6566258143d90008540ece210e9
SoSo <LuckilyChensy@163.com>
2021-01-19 21:35:07 +0800

    leetcode - no125 验证回文串

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0125/Solution03.java
@@ -0,0 +1,35 @@
+package com.leetcode.no0125;
+
+public class Solution03 {
+
+    // Time O(n) Space O(n)
+    public boolean isPalindrome(String s) {
+
+        int n = s.length();
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right){
+
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                ++left;
+            }
+
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                --right;
+            }
+
+            if(left < right){
+                if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                    return false;
+                }
+                ++left;
+                --right;
+            }
+        }
+
+        return true;
+
+    }
+
+}
