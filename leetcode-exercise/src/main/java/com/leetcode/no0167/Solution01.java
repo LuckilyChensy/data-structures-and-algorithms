4ef9ca515bb90a04917cac86ce4f0fd703b4a8f2
SoSo <LuckilyChensy@163.com>
2021-01-20 22:54:39 +0800

    leetcode - no167 两数之和 II - 输入有序数组

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0167/Solution01.java
@@ -0,0 +1,21 @@
+package com.leetcode.no0167;
+
+public class Solution01 {
+
+    public int[] twoSum(int[] numbers, int target) {
+
+        int left = 0;
+        int right = numbers.length -1 ;
+
+        while(left < right){
+            if(numbers[left] + numbers[right] > target){
+                --right;
+            }else if(numbers[left] + numbers[right] < target){
+                ++left;
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+
+}
