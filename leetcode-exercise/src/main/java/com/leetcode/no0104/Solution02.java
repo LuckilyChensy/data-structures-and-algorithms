5297455883bbdbd8d385db269130ab3795cc4489
SoSo <LuckilyChensy@163.com>
2021-01-30 23:33:47 +0800

    leetcode 104 树的最大深度

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0104/Solution02.java
@@ -0,0 +1,35 @@
+package com.leetcode.no0104;
+
+
+import com.leetcode.bean.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution02 {
+
+    public int maxDepth(TreeNode root) {
+
+        if(root == null){
+            return 0;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int depth = 0;
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0 ; i < size; i++){
+                TreeNode s = q.poll();
+                if(s.left != null){q.add(s.left);}
+                if(s.right != null){q.add(s.right);}
+            }
+            ++depth;
+        }
+
+        return depth;
+
+    }
+
+}
