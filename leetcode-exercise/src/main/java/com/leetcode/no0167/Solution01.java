4fae83896394b299199632263ab40bc4524c6411
SoSo <LuckilyChensy@163.com>
2021-01-30 08:34:08 +0800

    leetcode 167 - 两数之和II - 输入有序数组

--- leetcode-exercise/src/main/java/com/leetcode/no0167/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0167/Solution01.java
@@ -12,6 +12,8 @@ public class Solution01 {
                 --right;
             }else if(numbers[left] + numbers[right] < target){
                 ++left;
+            }else{
+                return new int[]{left+1,right+1};
             }
         }
 
