d7848cf644ca2029cee2f4191312ca527197b24a
SoSo <LuckilyChensy@163.com>
2021-01-30 22:49:25 +0800

    leetcode 53 最大子序列和

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0053/Solution02.java
@@ -0,0 +1,21 @@
+package com.leetcode.no0053;
+
+public class Solution02 {
+
+    // Times : O(n) Space: O(1)
+
+    public int maxSubArray(int[] nums){
+
+        int pre = 0;
+        int maxAns = nums[0];
+
+        for(int x:nums){
+            pre = Math.max(pre+x,x);
+            maxAns = Math.max(maxAns,pre);
+        }
+
+        return maxAns;
+
+    }
+
+}
