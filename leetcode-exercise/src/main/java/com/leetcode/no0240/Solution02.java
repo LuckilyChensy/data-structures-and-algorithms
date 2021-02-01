9c5eac48fb4f0a1ae171c2bc8c71d3dcc3e493b5
SoSo <LuckilyChensy@163.com>
2021-02-01 20:06:11 +0800

    leetcode 240 搜索二维矩阵2

--- leetcode-exercise/src/main/java/com/leetcode/no0240/Solution02.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0240/Solution02.java
@@ -3,26 +3,26 @@ package com.leetcode.no0240;
 public class Solution02 {
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length ==0){
-            return true;
+        // 边界条件
+        if(matrix == null || matrix.length == 0 ||matrix[0] == null || matrix[0].length == 0){
+            return false;
         }
+        // 长度
+        int row = matrix.length;
+        int col = matrix[0].length;
+        // 索引值
+        int i = 0;
+        int j = col - 1;
 
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        int i = 0 ;
-        int j = n - 1;
-
-        while(i < m && j >= 0){
-            if(target < matrix[i][j]){
+        while( i < row && j >= 0){
+            if(matrix[i][j] > target){
                 --j;
-            }else if (target > matrix[i][j]){
+            }else if(matrix[i][j] < target){
                 ++i;
             }else{
                 return true;
             }
         }
-
         return false;
     }
 }
