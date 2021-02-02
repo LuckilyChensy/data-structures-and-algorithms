9e9950c6b6b99413342a57b04115c3947c997063
SoSo <LuckilyChensy@163.com>
2021-02-02 22:56:06 +0800

    leetcode 234 回文链表

--- leetcode-exercise/src/main/java/com/leetcode/no0234/Solution04.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0234/Solution04.java
@@ -4,25 +4,22 @@ import com.leetcode.bean.ListNode;
 
 public class Solution04 {
 
-    public boolean isPalindrome(ListNode head){
+    // 回文数反转之后不变，一个正向计算哈希值，一个反向计算哈希值，比较反转之前和反转之后的哈希值是否相等
+    public boolean isPalindrome(ListNode head) {
+
+        long hash1 = 0;
+        long hash2 = 0;
+        long h = 1;
+        long seed = (long)(1e9+7);
 
         ListNode cur = head;
-
-        int base = 11;
-        int mod = 1000000007;
-
-        int left = 0;
-        int right = 0;
-        int mul = 1;
-
         while(cur != null){
-            left = (int)(((long)left * base + cur.val)%mod);
-            right = (int)((right + (long)mul * cur.val)%mod);
-            mul = (int)((long)mul * base % mod);
+            hash1 = hash1 * seed + cur.val;
+            hash2 = hash2 + cur.val * h;
+            h *= seed;
             cur = cur.next;
         }
-
-        return left == right;
+        return hash1 == hash2;
     }
 
 }
