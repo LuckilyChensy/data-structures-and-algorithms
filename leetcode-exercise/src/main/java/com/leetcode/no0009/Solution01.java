65ded0be1c4eaae23bbc1363d4b7befd36bea59d
SoSo <LuckilyChensy@163.com>
2021-02-02 22:30:54 +0800

    leetcode 9 回文数字

--- leetcode-exercise/src/main/java/com/leetcode/no0009/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0009/Solution01.java
@@ -1,10 +1,23 @@
 package com.leetcode.no0009;
 
-public class Solution01 {
+public class  Solution01 {
 
     public boolean isPalindrome(int x) {
-        String reversedStr = (new StringBuilder(x + "")).reverse().toString();
-        return (x + "").equals(reversedStr);
+        // 将数字转成字符串
+        String str = String.valueOf(x);
+
+        int i = 0;
+        int j = str.length() - 1;
+
+        while(i < j){
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            ++i;
+            --j;
+        }
+
+        return true;
     }
 
 }
