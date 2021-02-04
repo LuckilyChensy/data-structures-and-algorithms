05beed12f73270307fb68101623b9f4e96884c46
SoSo <LuckilyChensy@163.com>
2021-02-05 00:21:40 +0800

    leetcode 53 最大子序和

--- leetcode-exercise/src/main/java/com/leetcode/no0053/Solution02.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0053/Solution02.java
@@ -2,20 +2,21 @@ package com.leetcode.no0053;
 
 public class Solution02 {
 
-    // Times : O(n) Space: O(1)
-
+    /**
+     * Times : O(n) Space: O(1)
+     * 从第 i 个元素起，前一段累计出负数的对后续的最值没有帮助，因此可以去掉
+     * */
     public int maxSubArray(int[] nums){
 
-        int pre = 0;
-        int maxAns = nums[0];
+        int max = Integer.MIN_VALUE;
+        int cur = 0;
 
-        for(int x:nums){
-            pre = Math.max(pre+x,x);
-            maxAns = Math.max(maxAns,pre);
+        for(int i = 0; i < nums.length; i++){
+            cur  = cur<0? nums[i]:(cur+nums[i]);
+            max = Math.max(cur,max);
         }
 
-        return maxAns;
-
+        return max;
     }
 
 }
