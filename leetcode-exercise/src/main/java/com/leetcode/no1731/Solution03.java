a1213d90aa7dbbf8fa1e6e8e9cf69c17decbfaac
SoSo <LuckilyChensy@163.com>
2021-02-05 20:32:14 +0800

    leetcode 剑指Offer03 数组中的重复元素

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1731/Solution03.java
@@ -0,0 +1,24 @@
+package com.leetcode.no1731;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution03 {
+
+    public int findRepeatNumber(int[] nums){
+
+        Set<Integer> set = new HashSet<Integer>();
+
+        int repeat = -1;
+
+        for(int num:nums){
+            if(!set.add(num)){
+                repeat = num;
+                break;
+            }
+        }
+
+        return repeat;
+    }
+
+}
