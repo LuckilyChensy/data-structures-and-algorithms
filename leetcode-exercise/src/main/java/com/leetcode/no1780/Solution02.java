69b0abac5e0ce43832f07c6d79c4c7f03fd4e391
SoSo <LuckilyChensy@163.com>
2021-02-05 23:41:24 +0800

    leetcode 剑指Offer10 递归解法

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1780/Solution02.java
@@ -0,0 +1,20 @@
+package com.leetcode.no1780;
+
+public class Solution02 {
+
+    public int fib(int n){
+
+        int a = 0;
+        int b = 1;
+        int sum;
+
+        for(int i = 0; i < n; i++){
+            sum = (a + b)%1000000007;
+            a = b;
+            b = sum;
+        }
+
+        return a;
+
+    }
+}
