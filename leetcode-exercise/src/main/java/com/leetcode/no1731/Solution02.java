8d28e4b35fb6c241f27e89c5feb4f7ce64df4365
SoSo <LuckilyChensy@163.com>
2021-02-05 20:39:58 +0800

    leetcode 剑指Offer03 数组中的重复元素

--- leetcode-exercise/src/main/java/com/leetcode/no1731/Solution02.java
+++ leetcode-exercise/src/main/java/com/leetcode/no1731/Solution02.java
@@ -3,25 +3,18 @@ package com.leetcode.no1731;
 public class Solution02 {
 
     public int findRepeatNumber(int[] nums) {
-
+        int temp;
         for(int i = 0 ; i < nums.length; i++){
-
             while(nums[i]!=i){
-
                 if(nums[i] == nums[nums[i]]){
                     return nums[i];
                 }
-
-                int temp = nums[i];
-                nums[i] = nums[nums[i]];
-                nums[nums[i]] = temp;
-
+                temp = nums[i];
+                nums[i] = nums[temp];
+                nums[temp] = temp;
             }
-
         }
-
         return -1;
-
     }
 
 }
