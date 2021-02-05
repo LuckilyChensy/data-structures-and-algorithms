1cac76f487c835933d5d8a08c6dde6589db524ee
SoSo <LuckilyChensy@163.com>
2021-02-05 21:48:21 +0800

    leetcode 剑指Offer06 从尾部遍历打印链表

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1777/Solution01.java
@@ -0,0 +1,28 @@
+package com.leetcode.no1777;
+
+import com.leetcode.bean.ListNode;
+
+import java.util.Stack;
+
+public class Solution01 {
+
+    public int[] reversePrint(ListNode head){
+
+        Stack<ListNode> stack = new Stack<ListNode>();
+        ListNode temp = head;
+
+        while(temp != null){
+            stack.push(temp);
+            temp = temp.next;
+        }
+
+        int size = stack.size();
+        int[] print = new int[size];
+
+        for(int i = 0; i < size; i++){
+            print[i] = stack.pop().val;
+        }
+
+        return print;
+    }
+}
