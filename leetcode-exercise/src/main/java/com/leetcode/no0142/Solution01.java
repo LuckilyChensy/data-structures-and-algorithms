3275c52a5345f918b1d58a936d8319ec9bd6f103
SoSo <LuckilyChensy@163.com>
2021-02-06 18:23:32 +0800

    leetcode 142 环形链表

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0142/Solution01.java
@@ -0,0 +1,20 @@
+package com.leetcode.no0142;
+
+import com.leetcode.bean.ListNode;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution01 {
+
+    public ListNode detectCycle(ListNode head){
+        Set<ListNode> set = new HashSet<>();
+        for(ListNode p = head; p != null; p = p.next){
+            if(set.contains(p)){
+                return p;
+            }
+            set.add(p);
+        }
+        return null;
+    }
+
+}
