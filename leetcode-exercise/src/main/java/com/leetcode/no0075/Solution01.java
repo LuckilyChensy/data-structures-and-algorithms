2674a15eb8f829424ee3a4a98c2014d62fa441aa
SoSo <LuckilyChensy@163.com>
2021-02-06 19:27:48 +0800

    leetcode 75 颜色分类

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0075/Solution01.java
@@ -0,0 +1,30 @@
+package com.leetcode.no0075;
+
+public class Solution01 {
+
+    public void sortColors(int[] nums) {
+
+        if(nums == null || nums.length == 0){ return; }
+
+        int cnt0 = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for(int num:nums){
+            if(num == 0){
+                ++cnt0;
+            }else if(num == 1){
+                ++cnt1;
+            }else{
+                ++cnt2;
+            }
+        }
+
+        int k = 0;
+        for(int i = 0; i < cnt0; ++i){ nums[k++] = 0; }
+        for(int i = 0; i < cnt1; ++i){ nums[k++] = 1; }
+        for(int i = 0; i < cnt2; ++i){ nums[k++] = 2; }
+
+    }
+
+}
