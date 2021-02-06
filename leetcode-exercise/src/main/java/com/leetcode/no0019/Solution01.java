26ce0e424607a021a8099dc119310e2d9a1efcdd
SoSo <LuckilyChensy@163.com>
2021-02-06 21:00:50 +0800

    leetcode 19  删除链表的倒数第 N 个节点

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0019/Solution01.java
@@ -0,0 +1,32 @@
+package com.leetcode.no0019;
+
+import com.leetcode.bean.ListNode;
+
+public class Solution01 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode p = dummy;
+        ListNode q = dummy;
+
+        for(;n>0&&q.next != null; --n){
+            q = q.next;
+        }
+
+        // 如果 n 超过链表的长度
+        if(n!=0){
+            return dummy.next;
+        }
+
+        // 两个指针相隔为n,一起往后移动
+        while(q.next != null){
+            p = p.next;
+            q = q.next;
+        }
+
+        p.next = p.next.next;
+        return dummy.next;
+    }
+}
