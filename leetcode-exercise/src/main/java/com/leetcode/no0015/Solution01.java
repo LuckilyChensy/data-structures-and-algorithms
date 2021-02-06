c9de9d81941582bba6858fec98528aff27ae3865
SoSo <LuckilyChensy@163.com>
2021-02-06 20:17:29 +0800

    leetcode 15 三数之和

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0015/Solution01.java
@@ -0,0 +1,24 @@
+package com.leetcode.no0015;
+
+import java.util.*;
+
+public class Solution01 {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                for(int k = j+1; k < nums.length; k++){
+                    List<Integer> elem = Arrays.asList(nums[i],nums[j],nums[k]);
+                    if(set.contains(elem)){ continue; }
+                    set.add(elem);
+                    result.add(elem);
+                }
+            }
+        }
+        return result;
+    }
+}
