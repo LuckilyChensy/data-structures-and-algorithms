e951d131b4b97f826b7c0fa20656cf40e8fd2a65
SoSo <LuckilyChensy@163.com>
2021-02-06 19:50:16 +0800

    leetcode 344 反转字符串

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0344/Solution01.java
@@ -0,0 +1,17 @@
+package com.leetcode.no0344;
+
+public class Solution01 {
+
+    public void reverseString(char[] s){
+
+        int n = s.length;
+
+        for(int left = 0, right = n - 1; left < right; ++left,--right){
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+        }
+
+    }
+
+}
