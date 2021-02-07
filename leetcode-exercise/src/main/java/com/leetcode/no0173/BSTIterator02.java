bbed4cb70d91ded1019cf559fcd329d3c3a06002
SoSo <LuckilyChensy@163.com>
2021-02-07 09:17:24 +0800

    leetcode 173 二叉搜索树迭代器

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0173/BSTIterator02.java
@@ -0,0 +1,36 @@
+package com.leetcode.no0173;
+
+import com.leetcode.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class BSTIterator02 {
+
+    Stack<TreeNode> stack;
+
+    public BSTIterator02(TreeNode root){
+        this.stack = new Stack<TreeNode>();
+        this.leftmostInorder(root);
+    }
+
+    private void leftmostInorder(TreeNode root){
+        while(root!=null){
+            this.stack.push(root);
+            root = root.left;
+        }
+    }
+
+    public int next(){
+        TreeNode topmostNode = this.stack.pop();
+        if(topmostNode.right != null){
+            this.leftmostInorder(topmostNode.right);
+        }
+        return topmostNode.val;
+    }
+
+    public boolean hasNext(){
+        return this.stack.size() > 0;
+    }
+
+}
