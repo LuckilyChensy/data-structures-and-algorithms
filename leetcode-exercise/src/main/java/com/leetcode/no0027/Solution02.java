600128459a39fcab4c6c043e9a13538ff4155d98
SoSo <LuckilyChensy@163.com>
2021-02-08 11:35:57 +0800

    leetcode 27 移动元素

--- leetcode-exercise/src/main/java/com/leetcode/no0027/Solution02.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0027/Solution02.java
@@ -1,4 +1,15 @@
 package com.leetcode.no0027;
 
 public class Solution02 {
+
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for(int j = 0 ;  j < nums.length; j++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
 }
