39340bcb0f1c2569856a06fa0d8eda580b48ef9a
SoSo <LuckilyChensy@163.com>
2021-02-08 13:04:30 +0800

    leetcode 303 区域和检索 - 数组不可变

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0303/NumArray02.java
@@ -0,0 +1,21 @@
+package com.leetcode.no0303;
+
+public class NumArray02 {
+
+    private int[] sum;
+
+    public NumArray02(int[] nums) {
+
+        sum = new int[nums.length + 1];
+
+        for(int i = 0; i < nums.length; i++){
+            sum[i+1] = sum[i] + nums[i];
+        }
+
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j+1] - sum[i];
+    }
+    
+}
