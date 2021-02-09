7fe3e3adcc7a844dd2f8e4b7679c728167df817a
SoSo <LuckilyChensy@163.com>
2021-02-10 00:14:53 +0800

    leetcode62 不同路径问题

--- leetcode-exercise/src/main/java/com/leetcode/no0055/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0055/Solution01.java
@@ -1,19 +1,27 @@
 package com.leetcode.no0055;
 
+/**
+ * 动态规划解法
+ */
 public class Solution01 {
 
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int rightmost = 0;
-        for(int i = 0; i < n; ++i){
-            if( i <= rightmost){
-                rightmost = Math.max(rightmost,i+nums[i]);
-                if(rightmost >= n-1){
-                    return true;
+
+        // 存在型问题的常用定义, 目的是记录每一个位置的可行性
+        boolean[] dp = new boolean[nums.length];
+        dp[0] = true;
+
+        for(int i = 1; i < nums.length; i++){
+            dp[i] = false;
+            for(int j = 0; j < i;j++){
+                if(dp[j] && nums[j]+j>=i){
+                    dp[i]  = true;
                 }
             }
         }
-        return false;
+
+        return dp[nums.length - 1];
+
     }
 
 }
