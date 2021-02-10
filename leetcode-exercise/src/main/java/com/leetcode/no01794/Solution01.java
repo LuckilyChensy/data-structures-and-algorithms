328d4d468d1f970d0ce8495fadc6bcf5756057ea
SoSo <LuckilyChensy@163.com>
2021-02-11 01:33:37 +0800

    leetcode 剪绳子

---
+++ leetcode-exercise/src/main/java/com/leetcode/no01794/Solution01.java
@@ -0,0 +1,18 @@
+package com.leetcode.no01794;
+
+public class Solution01 {
+
+    public int cuttingRope(int n) {
+
+        if (n < 3) { return n - 1; }
+
+        int a = n / 3;
+        int b = n % 3;
+
+        if (b == 0) { return (int) Math.pow(3, a); }
+        if (b == 1) { return (int) Math.pow(3, a - 1) * 4; }
+        return (int) Math.pow(3, a) * 2;
+
+    }
+}
+
