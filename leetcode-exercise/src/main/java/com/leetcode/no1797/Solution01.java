285e7b82e8b333ef177cca600b3d27c7ec55db04
SoSo <LuckilyChensy@163.com>
2021-02-11 13:44:44 +0800

    leetcode 剑指offer 1到位数n的数

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1797/Solution01.java
@@ -0,0 +1,19 @@
+package com.leetcode.no1797;
+
+public class Solution01 {
+
+    public int[] printNumbers(int n){
+
+        int end = (int)Math.pow(10,n) - 1;
+
+        int[] res = new int[end];
+
+        for(int i = 0; i < end; i++){
+            res[i] = i + 1;
+        }
+
+        return res;
+
+    }
+
+}
