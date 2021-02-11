08c792e9839923083bf7b98c0d4a8b4ec3214d46
SoSo <LuckilyChensy@163.com>
2021-02-11 11:04:13 +0800

    leetcode 剪绳子

---
+++ leetcode-exercise/src/main/java/com/leetcode/no01794/Solution02.java
@@ -0,0 +1,22 @@
+package com.leetcode.no01794;
+
+public class Solution02 {
+
+    public int cuttingRope(int n) {
+
+        // 遍历各段不同的跨越间距对结果的影响
+        int[] dp = new int[n+1];
+
+        dp[2] = 1;
+
+        for(int i = 3; i <= n; i++){
+            for(int j = 1; j <= i-2;j++){
+                dp[i] = Math.max(Math.max(dp[i-j],i-j)*j,dp[i]);
+            }
+        }
+
+        return dp[n];
+    }
+
+}
+
