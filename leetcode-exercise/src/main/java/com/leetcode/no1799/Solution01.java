39c28bc5f5be329693f781dd49b3ec1f9b97b74f
SoSo <LuckilyChensy@163.com>
2021-02-11 15:16:44 +0800

    leetcode 剑指offer 删除链表节点

--- leetcode-exercise/src/main/java/com/leetcode/no1799/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no1799/Solution01.java
@@ -6,13 +6,17 @@ public class Solution01 {
 
     public ListNode deleteNode(ListNode head, int val){
 
+        if(head == null){ return null; }
+        if(head.val == val){return head.next;}
+
         ListNode cur = head;
 
-        while(cur.next != null){
-            if(cur.next.val == val){
-                cur.next = cur.next.next;
-            }
-            cur = cur.next;
+        while(cur.next!=null&&cur.next.val!=val){
+         cur = cur.next;
+        }
+
+        if(cur.next != null){
+            cur.next = cur.next.next;
         }
 
         return head;
