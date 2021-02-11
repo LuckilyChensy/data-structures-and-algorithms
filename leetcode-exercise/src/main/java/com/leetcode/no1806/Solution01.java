a630a4d492ce4c1f70e273472dc4ead70f211d59
SoSo <LuckilyChensy@163.com>
2021-02-12 01:04:49 +0800

    leetcode 剑指offer 树的子结构

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1806/Solution01.java
@@ -0,0 +1,23 @@
+package com.leetcode.no1806;
+
+import com.leetcode.bean.TreeNode;
+
+public class Solution01 {
+
+    public boolean isSubStructure(TreeNode A, TreeNode B){
+        return (A!=null&&B!=null)&&(recur(A,B)||isSubStructure(A.left,B)||isSubStructure(A.right,B));
+    }
+
+    boolean recur(TreeNode A,TreeNode B){
+
+        if(B == null){
+            return true;
+        }
+
+        if(A == null || A.val != B.val){
+            return false;
+        }
+
+        return recur(A.left,B.left)&&recur(A.right,B.right);
+    }
+}
