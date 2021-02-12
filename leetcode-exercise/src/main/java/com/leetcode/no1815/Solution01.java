ccb9604c10ecac4a40f85e8b3a7b32df6d490191
SoSo <LuckilyChensy@163.com>
2021-02-13 01:44:03 +0800

    leetcode 剑指offer 二叉树的后序遍历序列

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1815/Solution01.java
@@ -0,0 +1,26 @@
+package com.leetcode.no1815;
+
+public class Solution01 {
+
+    public boolean verifyPostorder(int[] postorder){
+        return recur(postorder,0,postorder.length - 1);
+    }
+
+    boolean recur(int[] postorder, int i, int j){
+        if(i >= j){ return true; }
+        int p = i;
+
+        while(postorder[p] < postorder[j]){
+            p++;
+        }
+
+        int m = p;
+
+        while(postorder[p] > postorder[j]){
+            p++;
+        }
+
+        return p ==j && recur(postorder,i,m-1) && recur(postorder,m,j-1);
+    }
+
+}
