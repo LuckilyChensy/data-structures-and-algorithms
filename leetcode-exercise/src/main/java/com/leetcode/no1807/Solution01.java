a0fbecf92f9d0f2b362893862c274f5f0e55f929
SoSo <LuckilyChensy@163.com>
2021-02-12 16:10:23 +0800

    leetcode 剑指offer 二叉树镜像

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1807/Solution01.java
@@ -0,0 +1,19 @@
+package com.leetcode.no1807;
+
+import com.leetcode.bean.TreeNode;
+
+public class Solution01 {
+
+    public TreeNode mirrorTree(TreeNode root){
+
+        if(root == null){ return null; }
+
+        TreeNode temp = root.left;
+        root.left = mirrorTree(root.right);
+        root.right = mirrorTree(temp);
+
+        return root;
+
+    }
+
+}
