a5544b4f995585baee030b7a7ccdc35f2c1f6e4a
SoSo <LuckilyChensy@163.com>
2021-02-12 16:40:05 +0800

    leetcode 剑指offer 二叉树镜像

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1808/Solution01.java
@@ -0,0 +1,17 @@
+package com.leetcode.no1808;
+
+import com.leetcode.bean.TreeNode;
+
+public class Solution01 {
+
+    public boolean isSymmetric(TreeNode root){
+        return root==null?true:recur(root.left,root.right);
+    }
+
+    boolean recur(TreeNode L,TreeNode R){
+        if(L == null && R == null){ return true; }
+        if(L == null || R == null){ return false; }
+        return (L.val == R.val)&&recur(L.left,R.right)&&recur(L.right,R.left);
+    }
+
+}
