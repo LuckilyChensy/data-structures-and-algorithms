f7c422b9b6274aa3b76b0df26bf69ebd2c7a04d8
SoSo <LuckilyChensy@163.com>
2021-02-13 23:40:32 +0800

    leetcode 283

--- leetcode-exercise/src/main/java/com/leetcode/no0283/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0283/Solution01.java
@@ -1,15 +1,20 @@
 package com.leetcode.no0283;
 
 public class Solution01 {
+
     public void moveZeros(int[] nums){
+
         int index = 0;
+
         for(int num:nums){
             if(num!=0){
                 nums[index++] = num;
             }
         }
+
         for(int i = index; i < nums.length; i++){
             nums[i] = 0;
         }
     }
+
 }
