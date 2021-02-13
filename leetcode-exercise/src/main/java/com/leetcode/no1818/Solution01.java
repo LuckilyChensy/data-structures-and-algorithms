51145a5be1909b2d873916af653102a0879baf29
SoSo <LuckilyChensy@163.com>
2021-02-13 15:47:09 +0800

    leetcode 剑指offer 二叉搜索树与双向链表

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1818/Solution01.java
@@ -0,0 +1,39 @@
+package com.leetcode.no1818;
+
+import com.leetcode.bean.Node;
+
+public class Solution01 {
+
+    // 主要解决好在改变节点指向后丢失节点的问题
+    Node head;
+    Node pre;
+
+    public Node treeToDoublyList(Node root){
+
+        if(root == null){
+            return null;
+        }
+
+        dfs(root);
+
+        pre.right = head;
+        head.left = pre;
+
+        return head;
+
+    }
+
+    public void dfs(Node cur){
+        if(cur == null) { return ; }
+        dfs(cur.left);
+
+        // pre 用于记录双向双向链表中位于 cur 左侧的节点, 即上一次迭代中的 cur, 当 pre == null 时, cur 左侧没有节点, 即此时 cur 为双向链表中的头节点
+        if(pre == null){ head = cur; }
+        else pre.right =cur;
+        cur.left = pre;
+        pre = cur;
+        dfs(cur.right);
+
+    }
+
+}
