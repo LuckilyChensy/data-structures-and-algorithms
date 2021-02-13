928219777baf9d7b9eb49ee44f56a86ed205474e
SoSo <LuckilyChensy@163.com>
2021-02-13 12:52:31 +0800

    leetcode 剑指offer 二叉树的后序遍历序列

--- leetcode-exercise/src/main/java/com/leetcode/no1815/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no1815/Solution01.java
@@ -7,20 +7,21 @@ public class Solution01 {
     }
 
     boolean recur(int[] postorder, int i, int j){
+
+        // 说明该节点为叶子节点
         if(i >= j){ return true; }
+
+        // 左子树起点, 遍历左子树与根节点之间的关系
         int p = i;
+        while(postorder[p] < postorder[j]){ p++; }
 
-        while(postorder[p] < postorder[j]){
-            p++;
-        }
-
+        // 右子树起点,遍历右子树与根节点之间的关系
         int m = p;
+        while(postorder[p] > postorder[j]){ p++; }
 
-        while(postorder[p] > postorder[j]){
-            p++;
-        }
-
+        // 左右子树都符合规定 && 对左子树验证 && 对右子树验证
         return p ==j && recur(postorder,i,m-1) && recur(postorder,m,j-1);
+
     }
 
 }
