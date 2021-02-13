37b6187aa16d3cac936659e3ece3d83f180986ce
SoSo <LuckilyChensy@163.com>
2021-02-13 17:31:15 +0800

    leetcode 剑指offer 二叉搜索树转化为循环链表

--- leetcode-exercise/src/main/java/com/leetcode/no1818/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no1818/Solution01.java
@@ -1,14 +1,14 @@
 package com.leetcode.no1818;
 
-import com.leetcode.bean.Node;
+import com.leetcode.bean.TreeNode;
 
 public class Solution01 {
 
     // 主要解决好在改变节点指向后丢失节点的问题
-    Node head;
-    Node pre;
+    TreeNode head;
+    TreeNode pre;
 
-    public Node treeToDoublyList(Node root){
+    public TreeNode treeToDoublyList(TreeNode root){
 
         if(root == null){
             return null;
@@ -23,13 +23,16 @@ public class Solution01 {
 
     }
 
-    public void dfs(Node cur){
+    public void dfs(TreeNode cur){
         if(cur == null) { return ; }
         dfs(cur.left);
 
         // pre 用于记录双向双向链表中位于 cur 左侧的节点, 即上一次迭代中的 cur, 当 pre == null 时, cur 左侧没有节点, 即此时 cur 为双向链表中的头节点
-        if(pre == null){ head = cur; }
-        else pre.right =cur;
+        if(pre == null){
+            head = cur;
+        }else {
+            pre.right =cur;
+        }
         cur.left = pre;
         pre = cur;
         dfs(cur.right);
