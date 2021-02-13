95ef606e869bebd055e9ee5d2ec3162382388825
SoSo <LuckilyChensy@163.com>
2021-02-13 15:01:32 +0800

    leetcode 剑指offer 复杂链表的复制

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1817/Solution01.java
@@ -0,0 +1,29 @@
+package com.leetcode.no1817;
+
+import com.leetcode.bean.Node;
+
+import java.util.HashMap;
+
+public class Solution01 {
+
+    public Node copyRandomList(Node head) {
+
+        HashMap<Node,Node> map = new HashMap<Node,Node>();
+        Node cur = head;
+
+        while(cur!=null){
+            map.put(cur,new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+        while(cur!=null){
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+
+        return map.get(head);
+    }
+
+}
