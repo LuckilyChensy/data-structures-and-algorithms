86fc2523eb432ac8829eeb39aac9c4465522b378
SoSo <LuckilyChensy@163.com>
2021-02-14 18:27:05 +0800

    leetcode 15 三数之和

--- leetcode-exercise/src/main/java/com/leetcode/no0015/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0015/Solution01.java
@@ -2,21 +2,30 @@ package com.leetcode.no0015;
 
 import java.util.*;
 
+/**
+ * 朴素解法
+ */
 public class Solution01 {
 
     public List<List<Integer>> threeSum(int[] nums) {
 
+        // 用于记录所有符合条件的结果
         List<List<Integer>> result = new ArrayList<>();
+
+        // set用于过滤重复的组合
         Set<List<Integer>> set = new HashSet<>();
+
         Arrays.sort(nums);
 
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i+1; j < nums.length; j++){
-                for(int k = j+1; k < nums.length; k++){
-                    List<Integer> elem = Arrays.asList(nums[i],nums[j],nums[k]);
-                    if(set.contains(elem)){ continue; }
-                    set.add(elem);
-                    result.add(elem);
+        for(int i = 0; i<nums.length-2; i++){
+            for(int j = i+1; j<nums.length-1; j++){
+                for(int k = j+1; k<nums.length; k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k]);
+                        if(set.contains(temp)){ continue; }
+                        set.add(temp);
+                        result.add(temp);
+                    }
                 }
             }
         }
