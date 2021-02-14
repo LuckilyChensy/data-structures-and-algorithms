5ec935bf15ec6e2ebed261b1d0c1fdc6c7720b2e
SoSo <LuckilyChensy@163.com>
2021-02-14 13:11:20 +0800

    leetcode 283 移动零

--- leetcode-exercise/src/main/java/com/leetcode/no0283/Solution02.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0283/Solution02.java
@@ -2,25 +2,24 @@ package com.leetcode.no0283;
 
 public class Solution02 {
 
+    // 冒泡的思维,将目标数据不断上升
     public void moveZeroes(int[] nums){
 
-        int n = nums.length;
-        int left = 0;
-        int right = 0;
+        int i = 0;  // 用于遍历所有位置
+        int j = 0;  // 用于指向0位置
+        int temp = 0;
 
-        while(right < n){
-            if(nums[right]!=0){
-                swap(nums,left,right);
-                left++;
+        // 若起始位不为零,i,j同步移动,原位交换,会遍历到第一次出现零的地方
+        while(i < nums.length){
+            if(nums[i]!=0){
+                temp = nums[i];
+                nums[i] =nums[j];
+                nums[j] = temp;
+                j++;
             }
-            right++;
+            i++;
         }
-    }
 
-    public void swap(int[] nums,int left,int right){
-        int temp = nums[left];
-        nums[left] = nums[right];
-        nums[right] = temp;
     }
 
 }
