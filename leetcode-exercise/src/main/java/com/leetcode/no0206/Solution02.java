17905e8fe1ea261df6656dd99fcf1925f8f3ce05
SoSo <LuckilyChensy@163.com>
2021-02-14 20:54:06 +0800

    leetcode 206 反转链表

--- leetcode-exercise/src/main/java/com/leetcode/no0206/Solution02.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0206/Solution02.java
@@ -4,16 +4,20 @@ import com.leetcode.bean.ListNode;
 
 public class Solution02 {
 
-    public ListNode reverseList(ListNode head){
+    public ListNode reverseList(ListNode head) {
 
+        // 边界条件翻盘 && 递归结束条件判断
         if(head == null || head.next == null){
             return head;
         }
 
-        ListNode newHead = reverseList(head.next);
+        ListNode cur = reverseList(head.next);
+        // 当前节点是 head , 利用栈保存上一节点的位置, 讲下一节点的指向反转
         head.next.next = head;
         head.next = null;
-        return newHead;
+
+        return cur;
+
     }
 
 }
