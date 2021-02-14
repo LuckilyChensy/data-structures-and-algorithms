cfa28b6d3494af92e7af3fec9a73a92e7d28b65c
SoSo <LuckilyChensy@163.com>
2021-02-14 15:39:37 +0800

    leetcode 1 两数之和

--- leetcode-exercise/src/main/java/com/leetcode/no0001/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0001/Solution01.java
@@ -1,30 +1,20 @@
 package com.leetcode.no0001;
 
+/**
+ * 朴素解法
+ */
 public class Solution01 {
 
-    public static int[] twoSum(int[] arr, int target){
-
-        int[] result = new int[2];
-
-        for (int i = 0; i < arr.length; i++) {
-            for(int j = i+1; j < arr.length; j++){
-               if(arr[i] + arr[j] == target){
-                   result[0] = i;
-                   result[1] = j;
-                   break;
-               }
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0 ; i < nums.length-1; i++){
+            int temp = target - nums[i];
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[j] == temp){
+                    return new int[]{i,j};
+                }
             }
         }
-
-        return result;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
-        int target = 9;
-        int[] result = twoSum(nums, target);
-        System.out.println(result[0]);
-        System.out.println(result[1]);
+        return new int[]{-1,-1};
     }
 
 }
