8440230263b544f972c05423db034c5ebce422e9
SoSo <LuckilyChensy@163.com>
2021-02-15 20:24:57 +0800

    leetcode 55 跳跃问题

--- leetcode-exercise/src/main/java/com/leetcode/no0055/Solution02.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0055/Solution02.java
@@ -1,19 +1,19 @@
 package com.leetcode.no0055;
 
 public class Solution02 {
+
     public boolean canJump(int[] nums) {
 
-        // 遍历每个节点后，能够着的最远的点
-        int futurestPoint = 0;
+       //当前节点下可以到的最远点
+        int max = 0;
 
-        for(int i = 0; i < nums.length; i++){
-            if(i > futurestPoint){
-                return false;
-            }else{
-                futurestPoint = Math.max(nums[i]+i,futurestPoint);
-            }
+        for(int i = 0 ; i < nums.length;i++){
+            if(max<i){ return false; }
+            max = Math.max(max,i+nums[i]);
         }
 
         return true;
+
     }
+
 }
