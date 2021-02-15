f1380e2409fcf1642f0e3d5bbfa0ae87a198221b
SoSo <LuckilyChensy@163.com>
2021-02-15 12:44:21 +0800

    leetcode 94 中序遍历

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0094/Solution01.java
@@ -0,0 +1,24 @@
+package com.leetcode.no0094;
+
+import com.leetcode.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution01 {
+
+    List<Integer> result = new ArrayList<Integer>();
+
+    public List<Integer> inorderTraversal(TreeNode root){
+
+        // 处理边界情况
+        if(root == null ){ return result; }
+
+        inorderTraversal(root.left);
+        result.add(root.val);
+        inorderTraversal(root.right);
+
+        return result;
+
+    }
+}
