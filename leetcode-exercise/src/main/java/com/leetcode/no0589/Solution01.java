a38578634f7f9a1431249b585b2e7b33832c1dc6
SoSo <LuckilyChensy@163.com>
2021-02-15 20:25:36 +0800

    leetcode 589 N叉树的前序遍历

--- leetcode-exercise/src/main/java/com/leetcode/no0589/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0589/Solution01.java
@@ -8,28 +8,28 @@ import java.util.Stack;
 
 public class Solution01 {
 
-    public List<Integer> preOrder(Node root){
-
-        List<Integer> result = new ArrayList<Integer>();
-        Stack<Node> stack = new Stack<Node>();
-
-        if(root == null){
-            return result;
-        }
-
-        stack.push(root);
-
-        while(!stack.isEmpty()){
-
-            Node temp = stack.pop();
-            result.add(temp.val);
-
-            for(int i = temp.children.size(); i >0; i--){
-                stack.push(root.children.get(i));
-            }
-
-        }
-
-        return result;
-    }
+//    public List<Integer> preOrder(Node root){
+//
+//        List<Integer> result = new ArrayList<Integer>();
+//        Stack<Node> stack = new Stack<Node>();
+//
+//        if(root == null){
+//            return result;
+//        }
+//
+//        stack.push(root);
+//
+//        while(!stack.isEmpty()){
+//
+//            Node temp = stack.pop();
+//            result.add(temp.val);
+//
+//            for(int i = temp.children.size(); i >0; i--){
+//                stack.push(root.children.get(i));
+//            }
+//
+//        }
+//
+//        return result;
+//    }
 }
