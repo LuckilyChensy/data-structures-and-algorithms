c2d1f84889264a21e3d332de343257ef142e870b
SoSo <LuckilyChensy@163.com>
2021-02-15 13:56:32 +0800

    leetcode 94 中序遍历

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0094/Solution02.java
@@ -0,0 +1,41 @@
+package com.leetcode.no0094;
+
+import com.leetcode.bean.ListNode;
+import com.leetcode.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution02 {
+
+    public List<Integer> inorderTraversal(TreeNode root){
+
+        List<Integer> result = new ArrayList<Integer>();
+
+        if(root == null){ return result; }
+
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        while(!stack.isEmpty() || cur!=null){
+
+            while(cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            TreeNode temp = stack.pop();
+
+            result.add(temp.val);
+
+            if(temp.right!=null){
+                cur = temp.right;
+            }
+
+        }
+
+        return result;
+
+    }
+}
