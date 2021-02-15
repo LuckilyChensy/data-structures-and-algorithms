66bd9fe283a115e81445e81bf85310c4ba8a53dd
SoSo <LuckilyChensy@163.com>
2021-02-15 14:03:10 +0800

    leetcode 144 前序遍历

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0144/Solution01.java
@@ -0,0 +1,25 @@
+package com.leetcode.no0144;
+
+import com.leetcode.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution01 {
+
+    List<Integer> result = new ArrayList<Integer>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        if(root == null){
+            return result;
+        }
+
+        result.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+
+        return result;
+    }
+
+}
