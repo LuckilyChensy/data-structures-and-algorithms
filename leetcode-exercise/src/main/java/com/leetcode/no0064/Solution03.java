f7c634186b01b4a94d5907c43d67b12ae2ecf019
SoSo <LuckilyChensy@163.com>
2021-02-16 15:30:07 +0800

    leetcode 64 不同路径

--- leetcode-exercise/src/main/java/com/leetcode/no0064/Solution03.java
+++ leetcode-exercise/src/main/java/com/leetcode/no0064/Solution03.java
@@ -1,6 +1,9 @@
 package com.leetcode.no0064;
 
+import java.util.Arrays;
+
 public class Solution03 {
+
     private int M;
     private int N;
     private int[][] memo;
@@ -21,10 +24,10 @@ public class Solution03 {
 
     private int dfs(int[][] grid, int r, int c) {
         //若越界，则认为不可达，距离为无穷大
-        if (r < 0 || r >= M || c < 0 || c >= N) return Integer.MAX_VALUE;
-        if (memo[r][c] > -1) return memo[r][c];
+        if (r < 0 || r >= M || c < 0 || c >= N) { return Integer.MAX_VALUE;}
+        if (memo[r][c] > -1) { return memo[r][c]; }
         //若到达终点，终点的贡献值是其本身
-        if (r == M - 1 && c== N - 1) return grid[M - 1][N - 1];
+        if (r == M - 1 && c== N - 1){ return grid[M - 1][N - 1]; }
         //右边的点到终点最短的路径
         int right = dfs(grid, r, c + 1);
         //下面的点到终点的最短路径
