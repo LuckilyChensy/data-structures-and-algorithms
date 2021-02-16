6a3b4ebd9c99a1c463485f3862377e4c45371b98
SoSo <LuckilyChensy@163.com>
2021-02-16 16:47:32 +0800

    leetcode 338 比特位计数

---
+++ leetcode-exercise/src/main/java/com/leetcode/no0338/Solution01.java
@@ -0,0 +1,17 @@
+package com.leetcode.no0338;
+
+public class Solution01 {
+
+    public int[] countBits(int num) {
+
+        int[] dp = new int[num+1];
+        dp[0] = 0;
+
+        for( int i = 1; i <= num; i++){
+            dp[i] = dp[i>>1] + (i%2);
+        }
+
+        return dp;
+    }
+
+}
