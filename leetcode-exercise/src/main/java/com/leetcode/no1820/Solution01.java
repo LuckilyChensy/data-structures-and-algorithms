79f9bac5e2df14705ff1cc54fcbbce93be127461
SoSo <LuckilyChensy@163.com>
2021-02-20 23:23:11 +0800

    leetcode 1820 从上到下打印二叉树2

---
+++ leetcode-exercise/src/main/java/com/leetcode/no1820/Solution01.java
@@ -0,0 +1,35 @@
+package com.leetcode.no1820;
+
+import bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution01 {
+
+    public List<List<Integer>> levelOrder(TreeNode root){
+
+        // 辅助队列
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        // 返回结果
+        List<List<Integer>> result = new LinkedList<>();
+
+        if(root!=null){ queue.add(root); }
+
+        while(!queue.isEmpty()){
+            List<Integer> tmp = new ArrayList<>();
+            for(int i = queue.size(); i > 0; i--){
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if(node.left!=null){ queue.add(node.left);}
+                if(node.right!=null){ queue.add(node.right);}
+            }
+            result.add(tmp);
+        }
+
+        return result;
+
+    }
+}
