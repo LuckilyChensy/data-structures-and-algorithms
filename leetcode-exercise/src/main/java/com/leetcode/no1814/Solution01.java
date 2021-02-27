c650c1ce4b9184c1f623d3125cfcd05852cab439
SoSo <LuckilyChensy@163.com>
2021-02-27 11:52:36 +0800

    leetcode 1814 剑指 Offer 16. 数值的整数次方

--- leetcode-exercise/src/main/java/com/leetcode/no1814/Solution01.java
+++ leetcode-exercise/src/main/java/com/leetcode/no1814/Solution01.java
@@ -9,35 +9,24 @@ import java.util.Queue;
 
 public class Solution01 {
 
-    public List<List<Integer>> levelOrder(TreeNode root){
+    public double myPow(double x, int n) {
 
-        Queue<TreeNode> queue = new LinkedList<>();
+         if( x == 1.0){ return x; }
 
-        List<List<Integer>> result = new ArrayList<>();
+         double result = 1.0;
 
-        if(root != null){
-            queue.add(root);
-        }
+         if(n < 0){
+             x=1/x;
+             n=-n;
+         }
 
-        while(!queue.isEmpty()){
-            // 存储各层节点
-            LinkedList<Integer> tmp = new LinkedList<>();
+         while(n!=0){
+             result *= x;
+             n--;
+         }
 
-            for(int i = queue.size(); i > 0; i--){
+         return result;
 
-                TreeNode node = queue.poll();
-
-                if(result.size() % 2 == 0){
-                     tmp.addLast(node.val);
-                }else{
-                    tmp.addFirst(node.val);
-                }
-
-                if(node.left != null){ queue.add(node.left); }
-                if(node.right != null){ queue.add(node.right); }
-            }
-            result.add(tmp);
-        }
-        return result;
     }
+
 }
