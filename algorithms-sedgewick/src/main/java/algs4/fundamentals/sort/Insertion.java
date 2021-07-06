c3c651ae1ec9c8deaae1065d3b1e1413e8c6c62e
LuckilyChensy <LuckilyChensy@163.com>
2021-07-06 23:32:52 +0800

    插入排序

--- algorithms-sedgewick/src/main/java/algs4/fundamentals/sort/Insertion.java
+++ algorithms-sedgewick/src/main/java/algs4/fundamentals/sort/Insertion.java
@@ -15,28 +15,27 @@ public class Insertion {
         int len = nums.length;
 
         for(int i = 1; i < len; i++){
-           for(int j = i; j>0 && nums[j] < nums[j-1]; j--){
-               swap(nums,j,j-1);
+
+            int cur = nums[i];
+            int j;
+            for(j = i; j>0 && nums[j-1] > cur; j--){
+               nums[j] = nums[j-1];
            }
+           nums[j] = cur;
         }
 
     }
 
-    private static void swap(int[] nums,int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
     public static void main(String[] args) throws Exception{
+
         int n = 10000;
-        int[] nums = SortHelper.generateRandomArray(n,0,n);
+        int[] nums = SortHelper.generateNearlyOrderedArray(n,10);
 
         long startTime = System.currentTimeMillis();
         sort(nums);
         long endTime = System.currentTimeMillis();
 
-        System.out.println( " 插入排序 is Sorted: "+SortHelper.isSorted(nums)+ " : " + (endTime-startTime) + "ms" );
+        System.out.println( "【插入排序 is Sorted】: "+SortHelper.isSorted(nums)+ " : 【" + (endTime-startTime) + "ms】" );
 
     }
 
