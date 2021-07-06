7d7a1bdb00a8610b2bc77b6766876f5f41f4db2b
LuckilyChensy <LuckilyChensy@163.com>
2021-07-06 20:58:37 +0800

    插入排序

---
+++ algorithms-sedgewick/src/main/java/algs4/fundamentals/sort/Insertion.java
@@ -0,0 +1,47 @@
+package algs4.fundamentals.sort;
+
+import algs4.fundamentals.tool.SortHelper;
+
+/**
+
+ */
+public class Insertion {
+    /**
+
+     * @param nums
+     */
+    public static void sort(int[] nums){
+
+        int len = nums.length;
+
+        for(int i = 1; i < len; i++){
+           for(int j = i; j>0; j--){
+               if(nums[j] < nums[j-1]){
+                   swap(nums,j,j-1);
+               }else{
+                   break;
+               }
+           }
+        }
+
+    }
+
+    private static void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) throws Exception{
+        int n = 10000;
+        int[] nums = SortHelper.generateRandomArray(n,0,n);
+
+        long startTime = System.currentTimeMillis();
+        sort(nums);
+        long endTime = System.currentTimeMillis();
+
+        System.out.println( " 插入排序 is Sorted: "+SortHelper.isSorted(nums)+ " : " + (endTime-startTime) + "ms" );
+
+    }
+
+}
